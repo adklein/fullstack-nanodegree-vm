9543fe24730d12f62c97185f56a7d4da1d769c89
Aaron Klein <aklein@kleinlyons.lacal>
2016-01-15 11:41:20 -0800

    git commit

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,12 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+conn = psycopg2.connect("dbname=forum")
+cur = conn.cursor()
+# DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +19,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    conn = psycopg2.connect("dbname=forum")
+    cur = conn.cursor()
+    cur.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in cur.fetchall()]
+    # posts.sort(key=lambda row: row['time'], reverse=True)
+    conn.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +34,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    conn = psycopg2.connect("dbname=forum")
+    cur = conn.cursor()
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    cur.execute("INSERT INTO posts(content) VALUES(%s)", (content,))
+    # DB.append((t, content))
+    conn.commit()
+    conn.close()
