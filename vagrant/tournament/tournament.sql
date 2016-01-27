d8d6646d98b3f8ab8ed2ae41587887772e4ed572
Aaron Klein <adklein@umich.edu>
2016-01-27 10:58:29 -0800

    fixed standings view to include players with no matches recorded

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,24 +6,28 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-DROP DATABASE tournament;
-DROP TABLE players;
-DROP TABLE matches;
-DROP VIEW wins;
-DROP VIEW losses;
-DROP VIEW standings;
+DROP VIEW IF EXISTS standings;
+DROP VIEW IF EXISTS wins;
+DROP VIEW IF EXISTS losses;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+DROP DATABASE IF EXISTS tournament;
+
+
+
+
 
 CREATE DATABASE tournament;
 \c tournament;
 CREATE TABLE players 
 (
-playerID int,
-name varchar(255)
+    playerID serial PRIMARY KEY,
+    name varchar(255)
 );
 
 CREATE TABLE matches
 (
-    mathchID int,
+    mathchID serial PRIMARY KEY,
     winnerID int,
     loserID int
 );
@@ -32,7 +36,7 @@ CREATE VIEW wins AS
     SELECT p.playerID,
     count(m.winnerID) AS w
     FROM players AS p
-    JOIN matches as m
+    LEFT JOIN matches as m
     ON p.playerID = m.winnerID
     GROUP BY p.playerID
     ORDER BY w desc;
@@ -41,17 +45,18 @@ CREATE VIEW losses AS
     SELECT p.playerID,
     count(m.loserID) AS l
     FROM players AS p
-    JOIN matches as m
+    LEFT JOIN matches as m
     ON p.playerID = m.loserID
     GROUP BY p.playerID
     ORDER BY l desc;
 
 CREATE VIEW standings AS
     SELECT p.playerID,
+    p.name,
     wins.w,
     losses.l
-    FROM players AS p JOIN wins
-        ON p.playerID = wins.playerID JOIN losses
+    FROM players AS p LEFT JOIN wins
+        ON p.playerID = wins.playerID LEFT JOIN losses
         ON p.playerID = losses.playerID
     GROUP BY p.playerID, wins.w, losses.l
     ORDER BY wins.w desc;
